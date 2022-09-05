eb9edb6690b68b63ae488b0086c81ec86321ad96
burhanrobbani <82148400+burhanrobbani@users.noreply.github.com>
2022-09-05 10:04:40 +0700

    first commit

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-const menuBar = document.querySelector('.menu-bar')
-const menuNav = document.querySelector('.menu-navigation')
-
-menuBar.addEventListener('click', function name(params) {
-    menuNav.classList.toggle('menu-active')
-})
